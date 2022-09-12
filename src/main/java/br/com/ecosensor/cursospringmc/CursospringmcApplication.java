d3ce47cfe949535ecf30bf855e4763283ba268ae
Danilo N. B <danilobenatti@gmail.com>
2022-09-12 11:56:40 -0300

    Primeiro commit - criado o projeto

---
+++ src/main/java/br/com/ecosensor/cursospringmc/CursospringmcApplication.java
@@ -0,0 +1,14 @@
+package br.com.ecosensor.cursospringmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringmcApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/br/com/ecosensor/cursospringmc/CursospringmcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ecosensor.cursospringmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
